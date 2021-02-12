279eb196d49b5a3f91cfd2ec7867e1e6c848ef2d
JinDogNOOB <mrriey00@naver.com>
2021-02-12 18:07:21 +0900

    init

---
+++ src/main/java/com/template/config/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.template.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    
+}
