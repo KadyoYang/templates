a60e8b2e9f67da7c83ced3d391a63211f9c6de51
양찬규 <mrriey00@naver.com>
2024-04-02 22:25:02 +0900

    nestjs ejs template

--- nestjs-ejs-orm/src/app.controller.ts
+++ nestjs-ejs-orm/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test')
+  @Render('test/test.ejs')
+  test(): { name: string; age: number } {
+    return { name: 'testName', age: 23 };
   }
 }
